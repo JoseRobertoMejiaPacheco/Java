b4909176281c15ca690e095fcdf768bd8f8adec8
José Roberto Mejía Pacheco <isscjrmpacheco@gmail.com>
2021-02-06 22:52:22 -0600

    Introducción
    Infrerencia de datos, tipos, métodos y secuencias de escape.

---
+++ Fundamentos/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,91 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jrmpacheco
+ */
+public class HolaMundo {
+
+    /*
+     * Funcion main permite ejecutar el programa como punto de inicio
+     */
+    public static void main(String args[]) {
+        System.out.println(HolaMundo.HolaMundo());
+        System.out.println(HolaMundo.CaracteresEspeciales());
+        Scanner EntradaPorConsola= new Scanner(System.in);
+        System.out.println("Escribe algo :  ");
+        ImprimirEntradaDeTeclado(EntradaPorConsola.nextLine());
+        HolaMundo.TiposPrimitivos();
+        
+    }
+
+    public static String HolaMundo() {
+        return "Hola Mundo";
+    }
+
+    public static String CaracteresEspeciales() {
+        //Inferencia de datos: con var detecta el tipo de dato asignado
+        var nuevaLinea = "\n";
+        var tabulador = "\t";
+        var retroceso = "\b";
+        var comillaSimple = "\'";
+        var comillaDoble = "\"";
+        String oracion = "El salto de linea"
+                + nuevaLinea + tabulador + "Tabuladorrrr" //retroceso borra un cararcter a la izquerda(3)
+                + retroceso + retroceso + retroceso + " "
+                + comillaSimple + "Hola Mundo" + comillaSimple + " "
+                + comillaDoble + "Hola Mundo" + comillaDoble;
+        return oracion;
+    }
+    
+    public static void ImprimirEntradaDeTeclado(String sacanner){
+        System.out.println(sacanner);
+    }
+    
+    public static void TiposPrimitivos() {
+        //Tipos  
+        byte MaxByte= Byte.MAX_VALUE;
+        byte MinByte= Byte.MIN_VALUE;
+        
+        short MaxShort= Short.MAX_VALUE;
+        short MinShort= Short.MIN_VALUE;
+        
+        char MaxChar=Character.MAX_VALUE;
+        char MinChar=Character.MIN_VALUE;
+        
+        int MaxInt = Integer.MAX_VALUE;
+        int MinInt = Integer.MIN_VALUE;
+        
+        //Tipos Flotante
+        long MaxLong= Long.MAX_VALUE;//Se tiene que agregar una L al final para evitar el error de literal superada
+        long MinLong= Long.MIN_VALUE;                
+        
+        float MaxFloat=Float.MAX_VALUE;
+        float MinFloat=Float.MIN_VALUE;
+        
+        double MaxDouble = Double.MAX_VALUE;
+        double MinDouble = Double.MIN_VALUE;
+        
+        //Valores Logicos
+        boolean MaxBoolean=Boolean.TRUE;
+        boolean MinBoolean=Boolean.FALSE;
+        System.out.println("----Rango de valores Primitivos----\n");
+        System.out.println("\tMínimo\tMáximo\n");
+        System.out.println("Byte"+"\t"+MaxByte+"\t"+MinByte);
+        System.out.println("Short"+"\t"+MaxShort+"\t"+MinShort);
+        System.out.println("Char"+"\t"+MaxChar+"\t"+MinChar);
+        System.out.println("Integer"+"\t"+MaxInt+"\t"+MinInt);
+        System.out.println("Long"+"\t"+MaxLong+"\t"+MinLong);
+        System.out.println("Float"+"\t"+MaxFloat+"\t"+MinFloat);
+        System.out.println("Boolean"+"\t"+MaxBoolean+"\t"+MinBoolean);
+        
+        
+    }
+}
